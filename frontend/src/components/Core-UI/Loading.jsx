2f8e7e468664e0c10b331447af977e01b40592e5
Piyawat <65160382@go.buu.ac.th>
2025-07-20 23:14:41 +0700

    add animation loading

---
+++ frontend/src/components/Core-UI/Loading.jsx
@@ -0,0 +1,14 @@
+// src/Core-UI/Loading.jsx
+const Loading = () => {
+  return (
+    <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
+      <div className="flex space-x-2">
+        <div className="w-4 h-4 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div className="w-4 h-4 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div className="w-4 h-4 bg-white rounded-full animate-bounce"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- frontend/src/components/Homepage/Uploadphoto.jsx
+++ frontend/src/components/Homepage/Uploadphoto.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Loading from "../Core-UI/Loading";
 
 export const Uploadphoto = ({ image }) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -8,7 +9,6 @@ export const Uploadphoto = ({ image }) => {
       setIsLoading(true);
       const formData = new FormData(); //ใช้สำหรับสร้าง form data เหมือนกับข้อมูลที่ถูกส่งจาก <form>
       formData.append("image", image.file); //image.file คือ ค่า (value) ที่จะส่งไป ซึ่งเป็น File object ที่ได้จาก input type="file"
-      // console.log(image.file);
 
       //ส่งข้อมูลไปยัง api
       const response = await fetch("http://localhost:3000/api/predict", {
@@ -16,24 +16,26 @@ export const Uploadphoto = ({ image }) => {
         body: formData, //ส่งข้อมูลรูปภาพไปยัง api
       });
 
-      if(response.ok){
+      if (response.ok) {
         const data = await response.json();
-        console.log("data:",data)
+        console.log("data:", data);
       }
-
     } catch (error) {
       console.error("เกิดข้อผิดพลาด", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className="">
+      {isLoading && <Loading />}
       <button
         type="submit"
         className="border p-2.5 w-auto m-2.5 rounded-xl bg-[#B9FF66] font-medium cursor-pointer"
         onClick={handleUpload}
       >
-        อัปโหลดรูปภาพ
+        วิเคราะห์รูปภาพ
       </button>
     </div>
   );
